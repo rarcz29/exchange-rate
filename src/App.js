83048344a395194e2ff3d086c25fcb159b5ddb99
rarcz29 <rafalcz29@gmail.com>
2020-12-25 18:53:47 +0100

    Create header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import Container from "@material-ui/core/Container";
+import Divider from "@material-ui/core/Divider";
 
 function App() {
   return (
     <div className="App">
-      <Container maxWidth="sm"></Container>
+      <Container>
+        <h1>Exchange Rate</h1>
+        <Divider />
+      </Container>
     </div>
   );
 }
