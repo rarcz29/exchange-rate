a6a316745bcbdbd627f49068e01af56717c21563
rarcz29 <rafalcz29@gmail.com>
2020-12-26 17:22:56 +0100

    Update header

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,19 +1,25 @@
 import React from "react";
 import Divider from "@material-ui/core/Divider";
-import Grid from "@material-ui/core/Grid";
+import Clock from "../components/Clock";
 import "./Header.css";
 
 const Header = ({ title }) => {
   return (
     <header>
-      <div class="container">
-        <div class="header-left-side">
-          <h1 className="title">{title}</h1>
-          <p className="current-time">Current time: some time</p>
+      <div className="container">
+        <div id="abcd">
+          <h1 className="margin-zero">{title}</h1>
+          <Clock
+            className="margin-zero"
+            text="Current time: "
+            locales="pl-PL"
+          />
         </div>
 
-        <div class="header-right-side">
-          <p>some text</p>
+        <div className="right-side">
+          <h2 className="margin-zero">Service status</h2>
+          <p className="margin-zero">Last updated 11:50:23 PM</p>
+          <p className="margin-zero">Next update in 31 sec</p>
         </div>
       </div>
       <Divider />
