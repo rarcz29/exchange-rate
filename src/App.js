87cb7a2df643f617909c4a95d91aa9c08d398ebf
rarcz29 <rafalcz29@gmail.com>
2021-01-23 19:19:40 +0100

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-import Container from "@material-ui/core/Container";
 import AppBar from "./components/AppBar";
 import Clock from "./components/Clock";
-import "./App.scss";
-import ExchangeForm from "./components/ExchangeForm";
+import "./App.css";
 
 function App() {
     return (
-        <Container>
-            <AppBar contentMargin="2rem 1rem">
-                <div className="flex-vertical app-bar-left">
+        <>
+            <AppBar>
+                <div className="topbar">
                     <h1>Exchange Rate</h1>
                     <Clock text="Current time: " />
                 </div>
-                <div className="flex-vertical app-bar-right">
-                    <h2>Service status</h2>
-                    <p>some data</p>
-                </div>
             </AppBar>
 
-            <main>
-                <ExchangeForm />
-            </main>
-        </Container>
+            <main className="container"></main>
+        </>
     );
 }
 
